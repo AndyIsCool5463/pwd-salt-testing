0279736e2e4c29d09f358852d12e4b02aadad361
AndyIsCool5463 <andyl5463@gmail.com>
2019-08-31 20:57:13 -0700

    Got basics down, env file available for dl

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const args = require("args");
 const colors = require("colors");
 const pwdnsalt = require("password-hash-and-salt");
@@ -5,19 +6,28 @@ const enmap = require("enmap");
 const DB = new enmap({
   name: "SUPERSAFEDB"
 });
-
-DB.defer(() => {
+DB.defer.then(() => {
   DB.ensure("pwd", []);
 });
 args
-  .option("user", "username")
-  .option("pass", "pwd")
-  .command("type", "SALT / CHECK", "salt");
+  .option(
+    "user",
+    "username",
+    "ABCBUHCljukahegawjkerhglkeqwrhlqukrhglqkjhrfglkjhl"
+  )
+  .option("pass", "pwd", "HIHLGJHKLHLKUHLKJHLKJHLK")
+  .option("type", "SALT / CHECK / DUMP", "42069")
+  .command("RUN", "runagsagjhals", ["r"]);
 
 const flags = args.parse(process.argv);
 if (flags.type == "salt") {
-  pwdnsalt(flags.pass).hash((err, hash) => {
+  pwdnsalt(flags.pass).hash(process.env.SALT, (err, hash) => {
     if (err) return console.log(colors.red(`ERROR: \n \n ${err}`));
+    if (
+      DB.get("pwd").filter(vendor => vendor["username"] === flags.user).length >
+      0
+    )
+      return console.error("CANNOT FUCK OFF");
     console.log(colors.green(`HASH IS: ${hash}`));
     console.log(colors.blue(`Storing User: ${flags.user} with hash: ${hash}`));
     DB.push("pwd", {
@@ -26,27 +36,36 @@ if (flags.type == "salt") {
     });
     console.log("Ensuring Hash was saved correctly...");
     let abd = DB.get("pwd");
-    let user = abd.filter(vendor => vendor["username"] === flag.user);
-    pwdnsalt(flag.pass).verifyAgainst(user.hash, function(error, verified) {
+    let user = abd.filter(vendor => vendor["username"] === flags.user);
+    console.log(user);
+    pwdnsalt(flags.pass).verifyAgainst(user[0].hash, function(error, verified) {
+      console.log(flags.pass, user.hash);
       if (error) throw new Error("Something went wrong!");
       if (!verified) {
         console.log("Don't try! We got you!");
       } else {
-        console.log(color.green("Verified!"));
+        console.log(colors.green("Verified!"));
       }
     });
   });
 }
 if (flags.type == "check") {
   let data = DB.get("pwd");
-  let user = abd.filter(e => e["username"] === flag.user);
-  if (!user) return console.log(color.red("No user found."));
-  pwdnsalt(flag.pass).verifyAgainst(user.hash, (err, verified) => {
+  let user = data.filter(e => e["username"] === flags.user);
+  if (!user || user.length == 0)
+    return console.log(colors.red("No user found."));
+  console.log(user);
+  pwdnsalt(flags.pass).verifyAgainst(user[0].hash, (err, verified) => {
+    console.log(flags.pass);
     if (err) throw new Error("something went wrong");
     if (!verified) {
-      console.log(color.red("WRONG PASSWORD TARD."));
+      console.log(colors.red("WRONG PASSWORD TARD."));
     } else {
       console.log("Hi!");
     }
   });
+}
+if (flags.type == "DUMP") {
+  DB.deleteAll();
+  console.log(colors.rainbow("Databased DUMPED! 👏👏👏👏"));
 }
